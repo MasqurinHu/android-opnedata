477cc3cd8344ff44b86d2b15112739cda6118e8e
Masqurin <Masqurin@icloud.com>
2017-08-04 15:42:39 +0800

    設定layout 及網址

--- app/src/main/java/idv/masqurin/opendata/MainActivity.java
+++ app/src/main/java/idv/masqurin/opendata/MainActivity.java
@@ -2,9 +2,13 @@ package idv.masqurin.opendata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  static  String MY_URL = "http://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json";
+    private Button btnConnect;
+    private TextView tvResul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
