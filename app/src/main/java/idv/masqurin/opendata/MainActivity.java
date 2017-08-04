5e2857f442a3fa2581e625704167aa458afd6718
Masqurin <Masqurin@icloud.com>
2017-08-04 16:20:55 +0800

    測試連接自己的php

--- app/src/main/java/idv/masqurin/opendata/MainActivity.java
+++ app/src/main/java/idv/masqurin/opendata/MainActivity.java
@@ -17,7 +17,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    private  static  String MY_URL = "http://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json";
+    private  static  String MY_URL = "https://masqurin.000webhostapp.com/FieldDispatch/newDevice.php";
     private Button btnConnect;
     private TextView tvResul;
 
