d6ef7b87f9317cbf0db8b6fc05cf109131f1ece6
Masqurin <Masqurin@icloud.com>
2017-08-04 15:46:29 +0800

    find view id 設定async

--- app/src/main/java/idv/masqurin/opendata/MainActivity.java
+++ app/src/main/java/idv/masqurin/opendata/MainActivity.java
@@ -1,5 +1,6 @@
 package idv.masqurin.opendata;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,9 +10,21 @@ public class MainActivity extends AppCompatActivity {
     private  static  String MY_URL = "http://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json";
     private Button btnConnect;
     private TextView tvResul;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
     }
+
+    private void findViews() {
+        tvResul = (TextView) findViewById(R.id.tvResult);
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+    }
+
+    private class GetOpenDataTask extends AsyncTask<String, Void, String> {
+        
+    }
+
 }
